e68a3b5d55abf17e9b3213ded97bb8dea580e7e7
andre <andre@knowhere.to>
2020-12-02 11:03:41 +0100

    reset

--- modelling_siamese_performer.py
+++ modelling_siamese_performer.py
@@ -99,7 +99,6 @@ class SiamesePerformer(nn.Module):
 
     def forward(self, x1: LongTensor, x2: LongTensor):
         embedding2 = self.get_embedding(x2["input_ids"], mask=x2["attention_mask"].bool())
-        del x2
         embedding1 = self.model(x1["input_ids"], mask=x1["attention_mask"].bool())
         loss_function = AMSLoss()
         return loss_function.calculate_loss(embedding1, embedding2)
