1056ce9f57bcbb4f58e379785aeb443ef615ea6e
andre <andre@knowhere.to>
2020-12-04 15:23:25 +0100

    fix ams loss

--- training.py
+++ training.py
@@ -26,9 +26,6 @@ if __name__ == "__main__":
     train_dataset = DataLoaderLaper(
         corpus.get_train() if not bool(int(os.environ.get("DOWNSAMPLE", 1))) else corpus.get_train()[0:5000])
 
-    dev_dataset = DataLoaderLaper(
-        corpus.get_dev() if not bool(int(os.environ.get("DOWNSAMPLE", 1))) else corpus.get_train()[0:1000])
-
     auto_encoder = SiamesePerformer(tokenizer.vocab_size).cuda()
 
     cmd_args = add_argument()
@@ -39,28 +36,29 @@ if __name__ == "__main__":
     for epoch in range(int(os.environ.get("EPOCHS", 1))):
         if model_engine.local_rank == 0:
             print(f"{datetime.now()} Epoch {epoch}")
-
+        losses = list()
         for i, data in enumerate(trainloader):
             model_engine.train()
             data = data_collector_deepspeed(data, tokenizer, model_engine.local_rank)
             loss = model_engine(**data)
             loss = loss.mean()
+            losses.append(loss.item())
             model_engine.backward(loss)
             model_engine.step()
             if model_engine.local_rank != 0:
                 continue
 
             if (i * epoch + i) % int(os.environ.get("STEPS_PER_PRINT")) == 0:
-                with torch.no_grad():
-                    batches = [dev_dataset[i:(i + 32)] for i in range(0, len(dev_dataset), 32)]
-                    losses = list()
-                    for batch in batches:
-                        bs_input = dict()
-                        for e in batch:
-                            bs_input.update(e)
-                        bs_input = data_collector_deepspeed(bs_input, tokenizer, model_engine.local_rank)
-                        loss = auto_encoder(**bs_input)
-                        losses.append(loss.item())
+                #with torch.no_grad():
+                #    batches = [train_dataset[i:(i + 32)] for i in range(0, len(train_dataset), 32)]
+                #    losses = list()
+                #    for batch in batches:
+                #        bs_input = dict()
+                #        for e in batch:
+                #            bs_input.update(e)
+                #        bs_input = data_collector_deepspeed(bs_input, tokenizer, model_engine.local_rank)
+                #        loss = auto_encoder(**bs_input)
+                #        losses.append(loss.item())
                 print(f"{datetime.now()} Epoch {epoch} iter {i} Loss {sum(losses) / len(losses)}")
                 model_engine.save_checkpoint(os.environ.get("OUTPUT_DIR"), (i * epoch + i))
 
