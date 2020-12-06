7c50d4f34d235b12bc45a0918044a1a8f6e46b70
andre <andre@knowhere.to>
2020-12-06 10:01:45 +0100

    add more power to env.list

--- setup.py
+++ setup.py
@@ -7,4 +7,9 @@ config["train_micro_batch_size_per_gpu"] = int(os.environ.get("BATCH_SIZE_PER_GP
 config["gradient_accumulation_steps"] = int(os.environ.get("GRADIENT_ACCUMULATION_STEPS", 100))
 config["train_batch_size"] = 10 * config["train_micro_batch_size_per_gpu"] * config["gradient_accumulation_steps"]
 config["steps_per_print"] = int(os.environ.get("STEPS_PER_PRINT", 100))
+
+config["optimizer"]["type"] = str(os.environ.get("OPTIMIZER_TYPE", "Adam"))
+config["optimizer"]["params"]["lr"] = float(os.environ.get("LEARNING_RATE", 0.0001))
+config["scheduler"]["params"]["warmup_max_lr"] = float(os.environ.get("LEARNING_RATE", 0.0001))
+config["scheduler"]["params"]["warmup_num_steps"] = int(os.environ.get("LEARNING_RATE", 500))
 json.dump(config, open("/workspace/ds_config.json", "w"))
